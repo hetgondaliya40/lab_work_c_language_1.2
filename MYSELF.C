aa992c5803bd42478f35bfd18e0cb677a08ffb8a
het gondaliya <hetgondaliya104@gmail.com>
2023-11-06 18:48:20 +0530

    first commit

---
+++ MYSELF.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+clrscr();
+printf("My name is het.....\n");
+printf("my age 18.....\n");
+printf("my school name is BIS.....\n");
+printf("my course name is flutter.....\n");
+printf("my gr id is 5782.....\n");
+printf("i am living in surat\n");
+getch();
+}
